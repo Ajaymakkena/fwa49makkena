0185c35a3d357b68226861c8d377172c36b7a0ce
Ajaymakkena <ajaymakkena5090@gmail.com>
2021-10-27 21:56:47 -0500

    modified

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ajay Makkena' });
+  res.render('mydata', { title: 'Ajay Makkena' });
 });
 
 module.exports = router;
